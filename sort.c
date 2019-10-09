3fffe4d513cf041005b3d99af22c0b418d10f0a6
Aileen Olen <aolen@mi-r5.21-school.ru>
2019-10-09 14:58:33 +0300

    time sort fixed and size sorting added

--- sort.c
+++ sort.c
@@ -33,7 +33,29 @@ l_file		**sort_by_time(l_file **struct_array)
 	{
 		j = i - 1;
 		temp = struct_array[i];
-		while (j >= 0 && ft_strcmp(struct_array[j]->time, temp->time) < 0)
+		while (j >= 0 && struct_array[j]->unix_time - temp->unix_time < 0)
+		{
+			struct_array[j + 1] = struct_array[j];
+			j--;
+		}
+		struct_array[j + 1] = temp;
+		i++;
+	}
+	return (struct_array);
+}
+
+l_file		**sort_by_size(l_file **struct_array)
+{
+	int i;
+	int j;
+	l_file	*temp;
+
+	i = 1;
+	while (struct_array[i])
+	{
+		j = i - 1;
+		temp = struct_array[i];
+		while (j >= 0 && struct_array[j]->file_size - temp->file_size > 0)
 		{
 			struct_array[j + 1] = struct_array[j];
 			j--;
