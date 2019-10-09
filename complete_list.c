1c0028113422a68f99dac6d4bbb532844943307c
Aileen Olen <aolen@mi-r5.21-school.ru>
2019-10-09 12:14:40 +0300

    Chmod splitted to 2 funcs

--- complete_list.c
+++ complete_list.c
@@ -1,6 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   complete_list.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aolen <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/09 11:50:19 by aolen             #+#    #+#             */
+/*   Updated: 2019/10/09 11:50:21 by aolen            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "ft_ls.h"
 #include "stdio.h"
+
+
+//static void		choose_chmod(int n, char *str)
+//{
+//	int i;
+//	char last3bits;
+//
+//	i = 3;
+//	while (i--)
+//	{
+//		last3bits = n & 7;
+//		n = n >> 3;
+//		if (last3bits == 0)
+//			chmod = ft_strjoin(chmod, "---");
+//		if (last3bits == 1)
+//			chmod = ft_strjoin(chmod, "--x");
+//		if (last3bits == 2)
+//			chmod = ft_strjoin(chmod, "-w-");
+//		if (last3bits == 3)
+//			chmod = ft_strjoin(chmod, "-wx");
+//		if (last3bits == 4)
+//			chmod = ft_strjoin(chmod, "r--");
+//		if (last3bits == 5)
+//			chmod = ft_strjoin(chmod, "r-x");
+//		if (last3bits == 6)
+//			chmod = ft_strjoin(chmod, "rw-");
+//		if (last3bits == 7)
+//			chmod = ft_strjoin(chmod, "rwx");
+//	}
+//}
 
 static l_file	*add_chmod(l_file *files, char *d_name)//struct dirent *dir)
 {
@@ -10,33 +51,34 @@ static l_file	*add_chmod(l_file *files, char *d_name)//struct dirent *dir)
 	int 			i;
 
 	stat(d_name, &fileStat);
-	chmod = "";
+	chmod = ft_strnew(12);
 	if (S_ISREG(fileStat.st_mode))
-		chmod = ft_strjoin(chmod, "-");
-	if (S_ISDIR(fileStat.st_mode))
-		chmod = ft_strjoin(chmod, "d");
-	if (S_ISLNK(fileStat.st_mode))
-		chmod = ft_strjoin(chmod, "x");
+		chmod = ft_strcat(chmod, "-");
+	else if (S_ISDIR(fileStat.st_mode))
+		chmod = ft_strcat(chmod, "d");
+	else if (S_ISLNK(fileStat.st_mode))
+		chmod = ft_strcat(chmod, "x");
+	//choose_chmod(fileStat.st_mode);
 	s = ft_atoi(ft_itoa_base(fileStat.st_mode, 8, 'a')) % 1000;
 	i = 100;
 	while (i != 0)
 	{
 		if (s / i % 10 == 0)
-			chmod = ft_strjoin(chmod, "---");
+			chmod = ft_strcat(chmod, "---");
 		if (s / i % 10 == 1)
-			chmod = ft_strjoin(chmod, "--x");
+			chmod = ft_strcat(chmod, "--x");
 		if (s / i % 10 == 2)
-			chmod = ft_strjoin(chmod, "-w-");
+			chmod = ft_strcat(chmod, "-w-");
 		if (s / i % 10 == 3)
-			chmod = ft_strjoin(chmod, "-wx");
+			chmod = ft_strcat(chmod, "-wx");
 		if (s / i % 10 == 4)
-			chmod = ft_strjoin(chmod, "r--");
+			chmod = ft_strcat(chmod, "r--");
 		if (s / i % 10 == 5)
-			chmod = ft_strjoin(chmod, "r-x");
+			chmod = ft_strcat(chmod, "r-x");
 		if (s / i % 10 == 6)
-			chmod = ft_strjoin(chmod, "rw-");
+			chmod = ft_strcat(chmod, "rw-");
 		if (s / i % 10 == 7)
-			chmod = ft_strjoin(chmod, "rwx");
+			chmod = ft_strcat(chmod, "rwx");
 		i /= 10;
 	}
 	chmod[10] = 0;
--- test.sh
+++ test.sh
@@ -1,7 +1,9 @@
 # ${1} flags variable with "-"
 # ${2} file variables
 gcc *.c Libft/*.c
-if [[ -n ${1} && -z ${2} ]]; then
+if [[ -n ${1} && ${1} = "-c" ]]; then
+  echo "compile"
+elif [[ -n ${1} && -z ${2} ]]; then
   ./a.out ${1} > user.output
   ls ${1} > system.output
 elif [[ -n ${2} && -n ${1} ]]; then
@@ -14,4 +16,6 @@ else
   ./a.out > user.output
   ls > system.output
 fi
-diff -u system.output user.output
+if ! [[ -n ${1} && ${1} = "-c" ]]; then
+  diff -u system.output user.output
+fi
